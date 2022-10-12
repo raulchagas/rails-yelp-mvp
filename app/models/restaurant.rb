f2015cb96ddd1cbe349e03751dc1eb9f44a85199
raul chagas <raul_chagas@hotmail.com>
2022-10-11 23:02:03 -0300

    models and routes done

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :name, :address, presence: :true
+
+end
